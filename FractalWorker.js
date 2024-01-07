e735afccdc01fed26cfd7ba3ffa3b538307d0b58
Bartłomiej Rasztabiga <bartlomiej.rasztabiga@symmetrical.ai>
2024-01-07 21:46:34 +0100

    Revert complex.js

--- FractalWorker.js
+++ FractalWorker.js
@@ -2,10 +2,7 @@ let palette = null;
 
 //functions return number from 0 to (maxIter-1)
 function julia(cx, cy, maxIter, cr, ci) {
-  let iter;
-
-  let xn;
-  let yn;
+  let iter, xn, yn;
 
   for (iter = 0; iter < maxIter; iter++) {
     xn = cx * cx - cy * cy + cr;
@@ -22,27 +19,19 @@ function julia(cx, cy, maxIter, cr, ci) {
 }
 
 const commands = {
-  palette: function (data, cb) {
+  palette: function (data, _) {
     palette = new Uint32Array(data.palette);
   },
-  render: function (data, cb) {
-    if (!palette) {
-      cb();
-      return;
-    }
-
+  render: function (data, callback) {
     const scale = Math.pow(2, data.z - 1);
     const x0 = data.x / scale - 1;
     const y0 = data.y / scale - 1;
     const d = 1 / (scale << 8);
     const pixels = new Array(65536);
     const MAX_ITER = data.maxIter;
-    let cx;
-    let cy;
-    let iter;
+
+    let cx, cy, px, py, iter;
     let i = 0;
-    let px;
-    let py;
 
     while (i < 65536) {
       px = i % 256;
@@ -54,7 +43,7 @@ const commands = {
     }
     const array = new Uint32Array(pixels);
     data.pixels = array.buffer;
-    cb(data, [data.pixels]);
+    callback(data, [data.pixels]);
   },
 };
 
